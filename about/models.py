7f889ca9617385e0372bb5dee90ca08f328a5efc
Brian Carter <brian@briancarter.dev>
2020-05-30 15:14:51 -0600

    Created about app with Bio Model using a WYSIWYG editor, registered with admin

---
+++ about/models.py
@@ -0,0 +1,18 @@
+from uuid import uuid4
+from django.db import models
+from tinymce.models import HTMLField
+
+
+class Bio(models.Model):
+    id = models.UUIDField(default=uuid4, editable=False,
+                          primary_key=True, unique=True)
+    content = HTMLField()
+
+    def __str__(self):
+        return self.content[0:15]
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.content = "".join(self.content.splitlines())
+
+        super().save(force_insert=force_insert, force_update=force_update,
+                     using=using, update_fields=update_fields)
--- resume/settings.py
+++ resume/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'tinymce',
+    'about.apps.AboutConfig',
     'education.apps.EducationConfig',
     'work.apps.WorkConfig',
 ]
